5210d25f52daee493d31e28ecd190397a7b167b5
MulanSales <luanmsacc@gmail.com>
2018-01-04 16:19:16 -0200

    Adicionado elementos da homepage #1

--- client/app.js
+++ client/app.js
@@ -6,6 +6,27 @@ registered using this mechanism.*/
 //The first argument is the name of the module (in this case Instagram)
 //and the second argument is an array of module dependencies(in this case ngRoute and ngMessages)
 angular.module('Instagram', ['ngRoute', 'ngMessages'])
-	.config(function($routeProvider)) {
-
-	});
+	.config(function($routeProvider) {
+		$routeProvider
+		/*when**() method above takes a relative URL path as its first argument and an object as 
+		its second argument. When we visit the index route "/"Angular will load home.html from 
+		the views directory and make HomeCtrl** controller available to that template.*/
+			.when('/', {
+				templateUrl: 'views/home.html',
+				controller: 'HomeCtrl'
+			})
+			.when('/login', {
+				templateUrl: 'views/login.html',
+				controller: 'LoginCtrl'
+			})
+			.when('/signuṕ', {
+				templateUrl: 'views/signuṕ.html',
+				controller: 'SignupCtrl'
+			})
+			//id tells it is a unique reference to a photo id
+			.when('/photo/:id', {
+				templateUrl: 'views/detail.html',
+				controller: 'DetailCtrl'
+			})
+			.otherwise('/');
+	});	
---
+++ client/controllers/home.js
@@ -0,0 +1,11 @@
+angular.module('Instagram')
+	.controller('HomeCtrl', function($scope) {
+
+		$scope.isAuthenticated = function(){
+
+		};
+
+		$scope.linkInstagram = function(){
+
+		};
+	});
