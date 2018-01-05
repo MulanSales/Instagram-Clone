c7114bcde0eb91c38a9d77335b02fd4a25571328
MulanSales <luanmsacc@gmail.com>
2018-01-04 22:29:29 -0200

    Adicionada API instagram #1 :

--- client/app.js
+++ client/app.js
@@ -5,8 +5,8 @@ registered using this mechanism.*/
 
 //The first argument is the name of the module (in this case Instagram)
 //and the second argument is an array of module dependencies(in this case ngRoute and ngMessages)
-angular.module('Instagram', ['ngRoute', 'ngMessages'])
-	.config(function($routeProvider) {
+angular.module('Instagram', ['ngRoute', 'ngMessages', 'satellizer'])
+	.config(function($routeProvider, $authProvider) {
 		$routeProvider
 		/*when**() method above takes a relative URL path as its first argument and an object as 
 		its second argument. When we visit the index route "/"Angular will load home.html from 
@@ -19,7 +19,7 @@ angular.module('Instagram', ['ngRoute', 'ngMessages'])
 				templateUrl: 'views/login.html',
 				controller: 'LoginCtrl'
 			})
-			.when('/signuṕ', {
+			.when('/signup', {
 				templateUrl: 'views/signuṕ.html',
 				controller: 'SignupCtrl'
 			})
@@ -29,4 +29,18 @@ angular.module('Instagram', ['ngRoute', 'ngMessages'])
 				controller: 'DetailCtrl'
 			})
 			.otherwise('/');
+
+			//Oauth provider for instagram
+			$authProvider.loginUrl = 'http://localhost:3000/auth/login';
+			$authProvider.signupUrl = 'http://localhost:3000/auth/signup';
+			$authProvider.oauth2({
+				name: 'instagram',
+				url: 'http://localhost:3000/auth/instagram',
+  				redirectUri: 'http://localhost:8000',
+  				clientId: '60b9e82ab4b44798af16c8ddf8eaf62c',
+				requiredUrlParams: ['scope'],
+				scope: ['likes'],
+				scopeDelimiter: '+',
+				authorizationEndpoint: 'https://api.instagram.com/oauth/authorize'
+			});
 	});	
--- client/controllers/home.js
+++ client/controllers/home.js
@@ -1,11 +1,17 @@
 angular.module('Instagram')
-	.controller('HomeCtrl', function($scope) {
+	.controller('HomeCtrl', function($scope, $window, $rootScope, $auth) {
 
 		$scope.isAuthenticated = function(){
-
+			return $auth.isAuthenticated();
 		};
 
 		$scope.linkInstagram = function(){
-
+			$auth.link('instagram')
+			.then(function(response) {
+				//Convert a JavaScript object into a string with JSON.stringify()
+				$window.localStorage.currentUser = JSON.stringfy(response.data.user);
+				//Parse the data with JSON.parse(), and the data becomes a JavaScript object
+				$rootScope.currentUser = JSON.parse($window.localStorage.currentUser);
+			});
 		};
 	});
